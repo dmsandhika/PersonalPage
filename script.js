ac4a095142d14ea5f8ae7502c531f762e6555bb0
Dimas Andhika Firmansyah <evandimas818@gmail.com>
2024-05-26 01:35:28 +0700

    first commint

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const themeSwitch = document.querySelector('.theme-switch__checkbox');
 const themeSwitchh = document.querySelector('.theme-switch_responsive');
 const themeStyle = document.getElementById('theme-style');
+const currentTheme = localStorage.getItem('theme');
 const image = document.getElementById('image');
 themeSwitch.addEventListener('change', function() {
   if (this.checked) {
@@ -11,15 +12,10 @@ themeSwitch.addEventListener('change', function() {
     image.src = 'img/myImg-light.png';
   }
 });
-themeSwitchh.addEventListener('change', function() {
-  if (this.checked) {
-    themeStyle.href = 'dark.css';
-    image.src = 'img/myImg-dark.png'; 
-  } else {
-    themeStyle.href = 'light.css'; 
-    image.src = 'img/myImg-light.png';
-  }
-});
+
+
+
+// Mengatur transisi antar halaman
 
 
 
