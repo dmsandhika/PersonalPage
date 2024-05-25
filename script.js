1811985de9a3658dbe348aacfa25a1c195d2b056
Dimas Andhika Firmansyah <evandimas818@gmail.com>
2024-05-26 04:06:08 +0700

    update navbar hp

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-const themeSwitch = document.querySelector('.theme-switch__checkbox');
-const themeSwitchh = document.querySelector('.theme-switch_responsive');
-const themeStyle = document.getElementById('theme-style');
-const currentTheme = localStorage.getItem('theme');
-const image = document.getElementById('image');
-themeSwitch.addEventListener('change', function() {
+const themeSwitch = document.querySelectorAll(".theme-switch__checkbox");
+const themeSwitchh = document.querySelectorAll(".theme-switch_responsive");
+const themeStyle = document.getElementById("theme-style");
+const currentTheme = localStorage.getItem("theme");
+const image = document.getElementById("image");
+themeSwitch.addEventListener("change", function () {
   if (this.checked) {
-    themeStyle.href = 'dark.css';
-    image.src = 'img/myImg-dark.png'; 
+    themeStyle.href = "dark.css";
+    image.src = "img/myImg-dark.png";
   } else {
-    themeStyle.href = 'light.css'; 
-    image.src = 'img/myImg-light.png';
+    themeStyle.href = "light.css";
+    image.src = "img/myImg-light.png";
   }
 });
 
-
-
 // Mengatur transisi antar halaman
-
-
-
-
-
